8e18ac79a7197f68f63f955b516a8ee7cdd62c59
Sergey994 <sergey.kalitsky@ukr.net>
2021-08-04 16:41:00 +0300

    Added views

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from groups.models import Group
 
-# Create your views here.
+
+def generate_groups(request):
+    # Generates groups by list
+    res = ''
+    group_names = ['python', 'java', 'VB']
+    for i in range(len(group_names)):
+        fake_group = {
+            'name': group_names[i - 1],
+            'type': i,
+        }
+        group = Group(**fake_group)
+        group.save()
+        res += str(group) + '<br>'
+    res += str(len(group_names)) + ' groups created.'
+    return HttpResponse(res)
+
+
+def view_groups(request):
+    # Returns all the groups in db
+    res = ''
+    for group in Group.objects.order_by('name'):
+        res += str(group) + '<br>'
+    return HttpResponse(res)
--- teachers/views.py
+++ teachers/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from teachers.models import Teacher
+from faker import Faker
 
-# Create your views here.
+
+def generate_teachers(request):
+    # Generates fake teachers in specified quantity, 1 in default
+    cnt = request.GET.get('count')
+    if cnt is None:
+        cnt = 1
+    elif cnt not in [str(i) for i in range(101)]:
+        return HttpResponse('Wrong input')
+    fake = Faker()
+    res = ''
+    for i in range(int(cnt)):
+        fake_teacher = {
+            'first_name': fake.first_name(),
+            'last_name': fake.last_name(),
+            'age': fake.random_int(min=18, max=100)
+        }
+        teacher = Teacher(**fake_teacher)
+        teacher.save()
+        res += str(teacher) + '<br>'
+    res += str(cnt) + ' teachers created.'
+    return HttpResponse(res)
+
+
+def view_teachers(request):
+    # Returns all teachers in db
+    res = ''
+    for teacher in Teacher.objects.order_by('first_name'):
+        res += str(teacher) + '<br>'
+    return HttpResponse(res)
